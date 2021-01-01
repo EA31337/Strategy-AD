af011e51b7e3bb65aa866e401357b7e30a04dc0e
kenorb <kenorb@users.noreply.github.com>
2021-01-01 00:01:56 +0000

    Optimize SignalOpenLevel (pf=0.25; dd=0.07; tt=73)

--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -10,7 +10,7 @@ struct Stg_AD_Params_M30 : StgParams {
     lot_size = 0;
     signal_open_method = -1;
     signal_open_filter = 1;
-    signal_open_level = 1;
+    signal_open_level = 40;
     signal_open_boost = 0;
     signal_close_method = -3;
     signal_close_level = 1;

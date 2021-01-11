593585fb1d8751a4741e52f38b60164ed84fc19c
kenorb <kenorb@users.noreply.github.com>
2021-01-11 07:27:30 +0000

    Optimize SignalClose params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -5,7 +5,9 @@
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_AD_Params_M15 : ADParams {
-  Indi_AD_Params_M15() : ADParams(indi_ad_defaults, PERIOD_M15) { shift = 0; }
+  Indi_AD_Params_M15() : ADParams(indi_ad_defaults, PERIOD_M15) {
+    shift = 0;
+  }
 } indi_ad_m15;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -17,10 +19,10 @@ struct Stg_AD_Params_M15 : StgParams {
     signal_open_filter = 24;
     signal_open_level = (float)7.0;
     signal_open_boost = 1;
-    signal_close_method = 0;
-    signal_close_level = (float)0.1;
+    signal_close_method = -4;
+    signal_close_level = (float)30.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)30.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -5,7 +5,9 @@
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_AD_Params_M5 : ADParams {
-  Indi_AD_Params_M5() : ADParams(indi_ad_defaults, PERIOD_M5) { shift = 0; }
+  Indi_AD_Params_M5() : ADParams(indi_ad_defaults, PERIOD_M5) {
+    shift = 0;
+  }
 } indi_ad_m5;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -17,10 +19,10 @@ struct Stg_AD_Params_M5 : StgParams {
     signal_open_filter = 8;
     signal_open_level = (float)1.0;
     signal_open_boost = 1;
-    signal_close_method = 0;
-    signal_close_level = (float)0.1;
+    signal_close_method = -1;
+    signal_close_level = (float)30.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)30.0;
     tick_filter_method = 1;
     max_spread = 0;
   }

f88cd818d751f1f40606e5b47b29ba1f87d15350
kenorb <kenorb@users.noreply.github.com>
2021-01-30 00:37:21 +0000

    Optimize SignalOpen params (pf=1.22; dd=0.06; tt=62)

--- config/EURUSD_M15.h
+++ config/EURUSD_M15.h
@@ -5,7 +5,9 @@
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_AD_Params_M15 : ADParams {
-  Indi_AD_Params_M15() : ADParams(indi_ad_defaults, PERIOD_M15) { shift = 0; }
+  Indi_AD_Params_M15() : ADParams(indi_ad_defaults, PERIOD_M15) {
+    shift = 0;
+  }
 } indi_ad_m15;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -13,14 +15,14 @@ struct Stg_AD_Params_M15 : StgParams {
   // Struct constructor.
   Stg_AD_Params_M15() : StgParams(stg_ad_defaults) {
     lot_size = 0;
-    signal_open_method = -4;
-    signal_open_filter = 24;
-    signal_open_level = (float)7.0;
+    signal_open_method = 1;
+    signal_open_filter = 8;
+    signal_open_level = (float)4.0;
     signal_open_boost = 1;
     signal_close_method = 0;
     signal_close_level = (float)0.1;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.1;
     tick_filter_method = 1;
     max_spread = 0;
   }
--- config/EURUSD_M30.h
+++ config/EURUSD_M30.h
@@ -5,7 +5,9 @@
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_AD_Params_M30 : ADParams {
-  Indi_AD_Params_M30() : ADParams(indi_ad_defaults, PERIOD_M30) { shift = 0; }
+  Indi_AD_Params_M30() : ADParams(indi_ad_defaults, PERIOD_M30) {
+    shift = 0;
+  }
 } indi_ad_m30;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -13,14 +15,14 @@ struct Stg_AD_Params_M30 : StgParams {
   // Struct constructor.
   Stg_AD_Params_M30() : StgParams(stg_ad_defaults) {
     lot_size = 0;
-    signal_open_method = -4;
+    signal_open_method = 1;
     signal_open_filter = 2;
-    signal_open_level = (float)0.1;
+    signal_open_level = (float)7.0;
     signal_open_boost = 1;
     signal_close_method = 0;
     signal_close_level = (float)0.1;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.1;
     tick_filter_method = 1;
     max_spread = 0;
   }

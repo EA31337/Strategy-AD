a886cb60304e55e6f3f1080c60d8958e3872b824
kenorb <kenorb@users.noreply.github.com>
2021-01-30 01:08:28 +0000

    Optimize PriceStop params (pf=10.78; dd=0.01; tt=6)

--- config/EURUSD_M5.h
+++ config/EURUSD_M5.h
@@ -5,7 +5,9 @@
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_AD_Params_M5 : ADParams {
-  Indi_AD_Params_M5() : ADParams(indi_ad_defaults, PERIOD_M5) { shift = 0; }
+  Indi_AD_Params_M5() : ADParams(indi_ad_defaults, PERIOD_M5) {
+    shift = 0;
+  }
 } indi_ad_m5;
 
 // Defines strategy's parameter values for the given pair symbol and timeframe.
@@ -19,8 +21,8 @@ struct Stg_AD_Params_M5 : StgParams {
     signal_open_boost = 1;
     signal_close_method = 0;
     signal_close_level = (float)0.1;
-    price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_method = 1;
+    price_stop_level = (float)0.1;
     tick_filter_method = 1;
     max_spread = 0;
   }

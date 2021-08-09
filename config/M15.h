1004c54ff8a1235996567dc43af54d2eafade787
kenorb <kenorb@users.noreply.github.com>
2021-08-09 21:20:29 +0100

    Renames signal open filter param as per the latest classes change

--- config/M15.h
+++ config/M15.h
@@ -14,7 +14,6 @@ struct Stg_AD_Params_M15 : StgParams {
   Stg_AD_Params_M15() : StgParams(stg_ad_defaults) {
     lot_size = 0;
     signal_open_method = 2;
-    signal_open_filter = 32;
     signal_open_level = (float)0.0;
     signal_open_boost = 1;
     signal_close_method = 2;
